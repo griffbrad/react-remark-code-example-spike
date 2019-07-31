e4f9b3252f6e375e11e6155904e35140b6b229f6
Brad Griffith <brad.griffith@automattic.com>
2019-07-31 11:49:59 -0500

    Spike demonstrating use of remark-react to generate a code block with a language attribute.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import PropTypes from 'prop-types'
+import unified from 'unified'
+import parse from 'remark-parse'
+import remark2react from 'remark-react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import githubSchema from 'hast-util-sanitize/lib/github.json';
+
+const schema = Object.assign({}, githubSchema, {
+  attributes: Object.assign({}, githubSchema.attributes, {
+    code: [
+      ...(githubSchema.attributes.code || []),
+      'className',
+	  'lang'
+    ]
+  })
+});
+
+function CustomCode( props ) {
+	console.log( props );
+
+	return (
+		<div>
+			Hello.
+		</div>
+	);
+}
+
+CustomCode.propTypes = {
+  className: PropTypes.string,
+  lang: PropTypes.string,
+  children: PropTypes.node
+};
+
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = { text: "```jsx\n<Button />\n```" }
+    this.onChange = this.onChange.bind(this)
+  }
+  onChange(e) {
+    this.setState({ text: e.target.value })
+  }
+  render() {
+    return (
+      <div>
+        <textarea value={this.state.text} onChange={this.onChange} />
+        <div id="preview">
+          {
+            unified()
+              .use(parse)
+              .use(
+			  	remark2react,
+				{
+					sanitize: schema,
+					remarkReactComponents: {
+						code: CustomCode
+					}
+				}
+			  )
+              .processSync(this.state.text).contents
+          }
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
